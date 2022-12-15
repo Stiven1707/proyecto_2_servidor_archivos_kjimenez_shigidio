28b26de302e317bd61ab15e92fc0f2f5e5e216e4
StibenDavid <108556627+StibenDavid@users.noreply.github.com>
2022-12-14 23:08:04 -0500

    Update protocol.c
    Clase totalmente documentada, LISTO

--- protocol.c
+++ protocol.c
@@ -34,38 +34,17 @@ int receive_file(int s, char *destination_folder)
 	char *ruta;
 	int dst_fd;
 	int faltantes;
-	// Recibe un archivo de un socket y lo almacena en destination_folder
 
-	// Primero se debe leer el socket el encabezado (de tipo file_info)
-	// que contiene la informacion del archivo:
-
-	// info.filename = nombre del archivo
-	// info.size = tamaño del archivo
-	// info.mode = modo (permisos).
-
-	// Se debe validar que se leyo el encabezado completo
-
-	// Luego se lee el contenido del archivo en bloques de BUFSIZ.
-
-	// Se debe tener en cuenta que el ultimo bloque (o el primero y unico, si el
-	// tamaño es menor que BUFSIZ) va a contener nulos al final que no deben ser
-	// escritos en el archivo de salida.
-
-	// =============
-
-	// 1. Leer la informacion del archivo desde el socket (estructura file_info)
-	// receive_file_info(s, info) //TODO validar!
+	// Lee la informacion del archivo desde el socket
+	// receive_file_info(s, info) 
 
 	if (!receive_file_info(s, &info))
 	{
 		perror("No se recibio el encabezado...");
 	}
 
-	// 2. Crear la ruta en la cual se almacena el archivo:
+	// Crea la ruta en la cual se almacena el archivo:
 
-	//	  destination_folder + "/" + file_info.filename
-	//    El modo se encuentra en file_info.mode
-	//  ruta=malloc(...)
 	if (!dir_exists(destination_folder))
 	{
 		if (mkdir(destination_folder, 0755) < 0)
@@ -83,15 +62,10 @@ int receive_file(int s, char *destination_folder)
 			//
 			printf("ruta donde recibo  %s\n", ruta);
 #endif
-	// 3. Abrir el archivo de salida
-
-	// Leer el contenido del archivo.
-	// La transferencia se realiza en bloques de BUFSIZ bytes
-	// Se debe prestar atencion al ultimo bloque, ya que puede ser
-	// de tamaño menor.
-	// En caso que el archivo sea pequeño, la transferencia se realizara en una
-	// sola lectura
-	dst_fd = open(ruta, O_CREAT | O_WRONLY | O_TRUNC, info.mode); // TODO validar!
+	//  Abre el archivo de salida
+	// Lee el contenido del archivo.
+	
+	dst_fd = open(ruta, O_CREAT | O_WRONLY | O_TRUNC, info.mode); 
 	if (dst_fd < 0)
 	{
 #ifdef DEBUG
@@ -100,35 +74,29 @@ int receive_file(int s, char *destination_folder)
 #endif
 		return 0;
 	}
-	// 4. faltantes = tamaño del archivo a recibir (tomado de la estructura file_info)
+	//faltantes = tamaño del archivo a recibir
 	faltantes = info.size;
 #ifdef DEBUG
 	//
 	printf("Cantidad recibida %d\n", faltantes);
 #endif
-	// 4.1 Mientras faltantes > 0
 	while (faltantes > 0)
 	{
 		int a_leer = BUFSIZ;
-		//			si a_leer > faltantes // Ultimo bloque
-		//				a_leer = faltantes
-		//			fin si
 		if (a_leer > faltantes)
 		{
 			a_leer = faltantes;
 		}
-		//      // Leer un bloque del socket
-		//			n_leidos = read(s, buf, a_leer) //TODO validar!
+		//      // Lee un bloque del socket
 		n_leidos = read(s, buff, a_leer);
 #ifdef DEBUG
 		//
 		printf("Cantidad leida %d\n", n_leidos);
 #endif
-		//		  si n_leidos > 0
+	
 		if (n_leidos > 0)
 		{
-			//       // Escribir el bloque al archivo
-			//				n_escritos = write(dst_fd, buf, n_leidos) //TODO validar!
+			//       // Escribe el bloque al archivo
 			n_escritos = write(dst_fd, buff, n_leidos);
 #ifdef DEBUG
 			//
@@ -139,22 +107,16 @@ int receive_file(int s, char *destination_folder)
 				fprintf(stderr, "Error la cantidad escrita %d es diferente a la leida %d", n_escritos, n_leidos);
 			}
 		}
-		//      fin si
-
-		//			faltaltes = faltantes - n_leidos
+		
 		faltantes = faltantes - n_leidos;
 #ifdef DEBUG
 		//
 		printf("Cantidad faltante %d\n", faltantes);
 #endif
-		//     Fin Mientras
 	}
-	// 5. Cerrar el archivo
-	// close(dst_fd)
+	// Cierra el archivo
 	close(dst_fd);
 
-	// 6. Retornar OK
-	// return 1
 	return 1;
 }
 
@@ -169,40 +131,15 @@ int send_file(int s, char *path)
 	int src_fd;
 	struct stat st;
 	int faltantes;
-	// Verificar si el archivo existe
-	// Si el archivo no existe, enviar un encabezado (file_info)
-	// con size = -1 y retornar 0
 
-	// Si el archivo existe y es un archivo regular,
-	// enviar primero el encabezado con la informacion del archivo:
-	// info.size = tamaño del archio
-	// info.filename = nombre el archivo (sin directorio)
-	// info.mode = modo del archivo
-
-	// El tamaño y el modo se obtienen con stat (2).
-
-	// Despues de enviar el encabezado, se debe enviar el contenido
-	// del archivo en bloques de BUFSIZ. Se debe tener en cuenta
-	// que el ultimo bloque (o el primero y unico, si el archivo es pequeño)
-	// llevara caracters nulos al final.
-
-	// ====================
-
-	// 1. Obtener el nombre del archivo de la ruta
-	// filename = basename(path)
+	// Obtiene el nombre del archivo de la ruta
 	filename = basename(path);
 
-	// Llenar los atributos de la estructura de tipo file_info
+	// Llena los atributos de la estructura de tipo file_info
 
-	// 2. Obtener la ruta absoluta del archivo
-	// realpath obtiene la ruta absolutam retornando NULL si no existe.
+	// Obtiene la ruta absoluta del archivo
 	ruta = realpath(path, NULL);
-	// ruta = realpath(path, NULL)
-	// si ruta == NULL
-	// info.size = -1
-	//  send_file_info(s, info) //TODO validar!
-	//  retornar 0 (falso)
-	// fin si
+	
 	if (ruta == NULL)
 	{
 		info.size = -1;
@@ -213,13 +150,9 @@ int send_file(int s, char *path)
 
 		return 0;
 	}
-	// 3. Verificar que la ruta corresponde a un archivo regular
-	// si stat(ruta, stat_s) != 0
+	// Verifica que la ruta corresponde a un archivo regular
 	if (stat(ruta, &st) != 0 || !S_ISREG(st.st_mode))
 	{
-		//  info.size = -1
-		//  send_file_info(s, info) // TODO validar!
-		//  retornar 0 (falso)
 		info.size = -1;
 		if (!send_file_info(s, &info))
 		{
@@ -227,77 +160,57 @@ int send_file(int s, char *path)
 		}
 		return 0;
 	}
-	// fin si
+	
 
-	// 4. Llenar los atributos del encabezado
+	// Llena los atributos del encabezado
 
-	// info.filename = filenane
 	strcpy(info.filename, filename);
-	// info.size = stat_s.st_size
 	info.size = st.st_size;
-	// info.mode = stat_s.st_mode
 	info.mode = st.st_mode;
 
-	// 5. Enviar el encabezado
+	// Envia el encabezado
 
-	// send_file_info(s, info)  //TODO validar!
+	
 	if (!send_file_info(s, &info))
 	{
 		perror("Error no se pudo enviar la informacion del archivo");
 	}
-	// 6. Abrir el archivo que se va a enviar
-	// src_fd = open(ruta, O_RDONLY) //TODO validar!
+	// Abre el archivo que se va a enviar
 	src_fd = open(ruta, O_RDONLY);
 	if (src_fd != -1)
 	{
-		// 7. Enviar el contenido archivo.
-		// La transferencia se realiza en bloques de BUFSIZ bytes
-		// Se debe prestar atencion al ultimo bloque, ya que puede ser
-		// de tamaño menor.
-		// En caso que el archivo sea pequeño, la transferencia se realizara en una
-		// sola lectura
-
-		// 8. faltantes = stat.st_size // Cantidad de bytes del archivo
+	
+		//Envia la informacion del archivo
 		faltantes = st.st_size;
 		while (faltantes > 0)
 		{
 			int a_leer = BUFSIZ;
-			//			si a_leer > faltantes // Ultimo bloque
-			//				a_leer = faltantes
-			//			fin si
+			
 			if (a_leer > faltantes)
 			{
 				a_leer = faltantes;
 			}
-			//      // Leer un bloque del socket
-			//			n_leidos = read(src_fd, buf, a_leer) //TODO validar!
+			//      // Lee un bloque del socket
 			n_leidos = read(src_fd, buff, a_leer);
 			//		  si n_leidos > 0
 			if (n_leidos > 0)
 			{
-				//       // Escribir el bloque al archivo
-				//				n_escritos = write(s, buf, n_leidos) //TODO validar!
+				//       // Escribe el bloque al archivo
 				n_escritos = write(s, buff, n_leidos);
 				if (n_escritos != n_leidos)
 				{
 					fprintf(stderr, "Error la cantidad escrita %d es diferente a la leida %d", n_escritos, n_leidos);
 				}
 			}
-			//      fin si
-
-			//			faltaltes = faltantes - n_leidos
+		
 			faltantes = faltantes - n_leidos;
-			//     Fin Mientras
 		}
 	}
 	else
 		return 0;
 
-	// 9. Cerrar el archivo
-	// close(src_fd)
+	// Cierra el archivo
 	close(src_fd);
-	// 10. Retornar OK
-	// return 1
 	return 1;
 }
 
